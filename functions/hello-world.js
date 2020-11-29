2b2af295654953f0c6f2978d616aa58cf1af8e5c
maximedinaux <maxime@dinaux.be>
2020-11-29 16:28:42 +0100

    Update hello-world.js

--- functions/hello-world.js
+++ functions/hello-world.js
@@ -1,8 +1,13 @@
 exports.handler = (event, context, callback) => {
+ const product = await stripe.products.create({
+  name: 'T-shirt',
+});
+
   const response = {
     statusCode: 200,
     body: JSON.stringify({
       message: 'Hi there!',
+      product : product,
       event: event,
     }),
   }
