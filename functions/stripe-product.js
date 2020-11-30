6ecb2f46600686ad9e65160c9f22918bb45e301b
maximedinaux <maxime@dinaux.be>
2020-11-30 08:24:06 +0100

    Update stripe-product.js

--- functions/stripe-product.js
+++ functions/stripe-product.js
@@ -1,19 +1,33 @@
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
 
 exports.handler = (event, context, callback) => {
-  const requestBody = JSON.parse(event.body)
-  const token = requestBody.token.id
 
-  return stripe.products
-    .create({
-      // Create Stripe charge with token
-      name: "T-shirt",
-      source: token,
-    })
-    .then(
-      callback(null, {
-        statusCode: 200,
-        body: "Product create",
-      })
-    )
+  const product = await stripe.products.create({
+    name: 'T-shirt',
+  });
+  const price = await stripe.prices.create({
+    product: '{{PRODUCT_ID}}',
+    unit_amount: 2000,
+    currency: 'usd',
+  });
+
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ['card'],
+    line_items: [{
+      price: '{{PRICE_ID}}',
+      quantity: 1,
+    }],
+    mode: 'payment',
+    success_url: 'https://localhost.com/?session_id={CHECKOUT_SESSION_ID}',
+    cancel_url: 'https://localhost.com/cancel',
+  });
+
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "Hi there!",
+      event: event,
+    }),
+  }
+  return callback(null, response)
 }
