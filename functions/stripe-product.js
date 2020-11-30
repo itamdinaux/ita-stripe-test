09e1b0a32de18798ad632465fd7890ba6e043943
maximedinaux <maxime@dinaux.be>
2020-11-30 07:58:36 +0100

    Update stripe-product.js

--- functions/stripe-product.js
+++ functions/stripe-product.js
@@ -3,16 +3,16 @@ exports.handler = (event, context, callback) => {
     const product = await stripe.products.create({
       name: "T-shirt",
     })
-    return product
+
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: "hello world",
+        product: product,
+        event: event,
+      }),
+    }
   }
 
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message : "hello world",
-      product: product,
-      event: event,
-    }),
-  }
   return callback(null, response)
 }
